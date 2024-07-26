cabd5bf4d6af36607230c33ccb5b6af3583f7927
VSeaman8 <127307143+VSeaman8@users.noreply.github.com>
2024-07-26 13:48:23 +0100

    ADDED Response to navbar

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -39,16 +39,6 @@ const Footer = () => {
           </li> */}
         </ol>
       </div>
-      <div className="photoCredit">
-        Photo by{" "}
-        <a href="https://unsplash.com/@brave4_heart?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">
-          Jigar Panchal
-        </a>{" "}
-        on{" "}
-        <a href="https://unsplash.com/photos/a-bunch-of-different-types-of-pipes-and-fittings-8BAyCFRCHqY?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">
-          Unsplash
-        </a>
-      </div>
     </footer>
   );
 };
